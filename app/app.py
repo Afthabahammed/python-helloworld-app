69eb55b9df19f710bd7c7d3d10e7342646d4057b
Afthab <afthabvadassery@gmail.com>
2026-08-11 15:26:36 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Deployment App")
 
+
 @app.get("/")
 def home():
     return {
@@ -11,7 +12,7 @@ def home():
         "message": "FastAPI deployed successfully!"
     }
 
+
 if __name__ == "__main__":
-    # Dynamically bind to the platform's assigned PORT and listen on all interfaces
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
